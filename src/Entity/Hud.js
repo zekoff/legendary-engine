f8d130340e8b3ed51272493fb06238bb45fae80a
Michael Zekoff <zekoff@gmail.com>
2017-09-09 18:58:08 +0000

    Add layer selection to HUD

--- src/Entity/Hud.js
+++ src/Entity/Hud.js
@@ -1,4 +1,11 @@
 /* global Phaser, phsr, game */
+var HEIGHT = 1000;
+var WIDTH = 1200;
+var layers = [{ name: 'military', color: 0xff0000 },
+    { name: 'commerce', color: 0x00ff00 },
+    { name: 'research', color: 0x0000ff }
+];
+
 var Hud = function() {
     Phaser.Group.call(this, phsr);
     this.x = 0;
@@ -7,6 +14,8 @@ var Hud = function() {
     this.bg.height = 200;
     this.bg.width = 1200;
     this.add(this.bg);
+
+    this.selectedLayer = null;
 
     this.planetText = phsr.add.text(5, 5, "PLANET");
     this.add(this.planetText);
@@ -38,6 +47,21 @@ var Hud = function() {
     });
     this.productionButtonText = phsr.add.text(605, 5, "INCREASE PRODUCTION");
     this.add(this.productionButtonText);
+
+    var i = 0;
+    layers.forEach(function(layer) {
+        var button = phsr.add.image(WIDTH, 5 + 65 * i++, 'pix');
+        button.anchor.set(1, 0);
+        button.width = 80;
+        button.height = 60;
+        button.tint = layer.color;
+        button.name = layer.name;
+        this.add(button);
+        button.inputEnabled = true;
+        button.events.onInputUp.add(function() {
+            this.setSelectedLayer(layer);
+        }, this);
+    }, this);
 };
 Hud.prototype = Object.create(Phaser.Group.prototype);
 Hud.constructor = Hud;
@@ -46,6 +70,36 @@ Hud.prototype.setSelectedPlanet = function(planet) {
     this.productionText.text = "Production Level: " + (planet ? planet.shipProductionRate : "NONE");
     this.orbitingText.text = "Ships in Orbit: " + (planet ? planet.orbitedBy.playerShips.length : "NONE");
     this.moneyText.text = "Money: " + game.money;
+};
+Hud.prototype.setSelectedLayer = function(layer) {
+    var button = this.getByName(layer.name);
+    var t;
+    // null layer (standard)
+    if (layer.name == this.selectedLayer) {
+        this.selectedLayer = null;
+        this.bg.tint = 0xffffff;
+        t = phsr.tweens.create(button);
+        t.to({ width: 80 }, 200);
+        t.onStart.add(function() { game.hud.ignoreChildInput = true; });
+        t.onComplete.add(function() { game.hud.ignoreChildInput = false; });
+        t.start();
+        print('selected layer: ', this.selectedLayer);
+        return;
+    }
+    // activate new layer
+    this.selectedLayer = layer.name;
+    // TODO this.bg.tint = layer.color;
+    t = phsr.tweens.create(button);
+    t.to({ width: 120 }, 200);
+    t.onStart.add(function() { game.hud.ignoreChildInput = true; });
+    t.onComplete.add(function() { game.hud.ignoreChildInput = false; });
+    t.start();
+    // collapse old layers, if applicable
+    layers.filter(function(e) { return e.name != layer.name; }, this)
+        .forEach(function(e) {
+            phsr.tweens.create(this.getByName(e.name)).to({ width: 80 }, 200, null, true);
+        }, this);
+    print('selected layer: ', this.selectedLayer);
 };
 
 module.exports = Hud;
